9d5c5154f61f7b26f7504656d1047084eb7d5cdf
Artyom Mikhailov <mihajlov.aa.00@yandex.ru>
2023-03-30 23:57:01 +0300

    Добавлены комментарии, отзывы и оценки

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import UserViewSet, get_token, profile, send_confirmation_code
+from .views import UserViewSet, get_token, profile, send_confirmation_code, ReviewViewSet, CommentViewSet
 
 router_v1 = SimpleRouter()
 router_v1.register('users', UserViewSet)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet, basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments'
+)
 
 
 urlpatterns = [
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,12 +13,12 @@ from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-from reviews.models import User
+from reviews.models import User, Review
 
 from .permissions import (AdminModeratorAuthorOnly, AdminOnly,
                           IsAdminUserOrReadOnly)
 from .serializers import (ProfileSerializer, SignUpSerializer, TokenSerializer,
-                          UserSerializer)
+                          UserSerializer, ReviewSerializer, CommentSerializer)
 
 HTTP_METOD_NAMES = ['get', 'post', 'head', 'delete', 'patch']
 
@@ -69,6 +69,33 @@ class UserViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ('username',)
     http_method_names = HTTP_METOD_NAMES
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [AdminModeratorAuthorOnly, ]
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(title=title, author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [AdminModeratorAuthorOnly, ]
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
 
 
 @api_view(['POST'])

d593075c66a2f2114c6e1e459dbb9afb938cbcf8
Артемий Абабков <art250706@gmail.com>
2023-03-30 21:43:32 +0500

    add models views serializers permissions

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
-# Create your views here.
+from reviews.models import Category, Genre, Title
+from .serializers import CategorySerializer, TitleSerializer, GenreSerializer
+from .permissions import IsAdminOrReadOnly
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', ]
+    lookup_field = 'slug'
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', ]
+    lookup_field = 'slug'
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [IsAdminOrReadOnly]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -29,3 +29,25 @@ class User(AbstractUser):
         unique=True,
         verbose_name='Адрес электронной почты'
     )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(null=True, blank=True)
+    description = models.CharField(max_length=1000, blank=True)
+    rating = models.IntegerField(blank=True, null=True)
+    genre = models.ManyToManyField(Genre, verbose_name='Жанр')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 blank=True, null=True,
+                                 verbose_name='Категория')
